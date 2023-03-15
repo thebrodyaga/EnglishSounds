89a2f8348bedb2b70c6874f1b45a35a3fd5a380a
Nikolai Emelianov <nikoly9393@gmail.com>
2023-03-15 14:27:10 +0300

    sound card item

--- brandbook/src/main/java/com/thebrodyaga/brandbook/component/sound/SoundCardUiModel.kt
+++ brandbook/src/main/java/com/thebrodyaga/brandbook/component/sound/SoundCardUiModel.kt
@@ -1,9 +1,13 @@
 package com.thebrodyaga.brandbook.component.sound
 
+import androidx.annotation.ColorRes
 import com.thebrodyaga.brandbook.recycler.model.UiModel
 import com.thebrodyaga.brandbook.recycler.model.UiModelPayload
+import com.thebrodyaga.core.uiUtils.text.TextViewUiModel
 
 data class SoundCardUiModel(
-
+    val transcription: TextViewUiModel,
+    val word: TextViewUiModel,
+    @ColorRes val transcriptionTint: Int,
     override val payload: Any? = null
 ) : UiModel, UiModelPayload
--- brandbook/src/main/java/com/thebrodyaga/brandbook/component/sound/SoundCardView.kt
+++ brandbook/src/main/java/com/thebrodyaga/brandbook/component/sound/SoundCardView.kt
@@ -1,13 +1,61 @@
 package com.thebrodyaga.brandbook.component.sound
 
+import androidx.core.content.ContextCompat
 import android.content.Context
 import android.util.AttributeSet
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.card.MaterialCardView
+import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+import com.thebrodyaga.brandbook.R
+import com.thebrodyaga.brandbook.databinding.ItemSoundCardBinding
+import com.thebrodyaga.brandbook.databinding.ViewSoundCardBinding
+import com.thebrodyaga.brandbook.recycler.model.UiModel
+import com.thebrodyaga.core.uiUtils.resources.px
+import com.thebrodyaga.core.uiUtils.text.bind
 
 class SoundCardView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : MaterialCardView(context, attrs) {
 
+    private val binding by viewBinding(ViewSoundCardBinding::bind)
+
+    private var _item: SoundCardUiModel? = null
+    val item: SoundCardUiModel
+        get() = _item ?: error("Not bind yet")
+
     init {
+        inflate(context, R.layout.view_sound_card, this)
+        val contentPadding = 16.px
+        setContentPadding(contentPadding, contentPadding, contentPadding, contentPadding)
+    }
+
+    fun setOnClickAction(onItemClickAction: (view: SoundCardView, item: SoundCardUiModel) -> Unit) {
+        setOnClickListener {
+            onItemClickAction.invoke(this, item)
+        }
+    }
+
+    fun bind(model: SoundCardUiModel) = with(binding) {
+        _item = model
+        sound.bind(model.transcription)
+        word.bind(model.word)
+        sound.backgroundTintList = ContextCompat.getColorStateList(context, model.transcriptionTint)
     }
 }
+
+fun soundCardDelegate(
+    inflateListener: ((soundCard: SoundCardView) -> Unit)? = null,
+    bindListener: ((view: SoundCardView, item: SoundCardUiModel) -> Unit)? = null,
+): AdapterDelegate<List<UiModel>> =
+    adapterDelegateViewBinding<SoundCardUiModel, UiModel, ItemSoundCardBinding>(
+        viewBinding = { inflater, parent -> ItemSoundCardBinding.inflate(inflater, parent, false) }
+    ) {
+
+        inflateListener?.invoke(binding.root)
+
+        bind {
+            binding.root.bind(item)
+            bindListener?.invoke(binding.root, item)
+        }
+    }
