2eac29cb948cd15b7c3585ab8467b9a0bf8b3516
Nikolai Emelianov <nikoly9393@gmail.com>
2023-04-04 15:56:59 +0300

    sound mouth image loading

--- feature/soundDetails/impl/src/main/java/com/thebrodyaga/feature/soundDetails/impl/ui/adapter/Delegates.kt
+++ feature/soundDetails/impl/src/main/java/com/thebrodyaga/feature/soundDetails/impl/ui/adapter/Delegates.kt
@@ -1,7 +1,9 @@
 package com.thebrodyaga.feature.soundDetails.impl.ui.adapter
 
 import android.view.View
+import androidx.core.view.doOnLayout
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
 import com.thebrodyaga.brandbook.model.UiModel
@@ -21,9 +23,12 @@ fun soundDetailsImageDelegate(
         inflateListener?.invoke(binding.root)
 
         bind {
-            Glide.with(context)
-                .load(File(context.filesDir, item.photoPath))
-                .into(binding.itemSoundDetailsImage)
+            binding.itemSoundDetailsImage.doOnLayout {
+                Glide.with(context)
+                    .load(File(context.filesDir, item.photoPath))
+                    .apply(RequestOptions().override(it.width, it.height))
+                    .into(binding.itemSoundDetailsImage)
+            }
             bindListener?.invoke(binding.root, item)
         }
     }
