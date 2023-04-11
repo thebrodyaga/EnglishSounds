4ff43c7e615c64932be4e5fd73479bf90107f42d
Nikolai Emelianov <nikoly9393@gmail.com>
2023-04-11 15:19:01 +0300

    diskCacheStrategy

--- legacy/src/main/java/com/thebrodyaga/legacy/YoutubeThumbnailView.kt
+++ legacy/src/main/java/com/thebrodyaga/legacy/YoutubeThumbnailView.kt
@@ -3,9 +3,11 @@ package com.thebrodyaga.legacy
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import androidx.core.net.toUri
 import androidx.core.view.doOnLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.thebrodyaga.legacy.databinding.ViewYoutubeThumbnailBinding
 
@@ -23,11 +25,12 @@ class YoutubeThumbnailView @JvmOverloads constructor(
         Glide.with(context)
             .load(getThumbnailUtl(videoId))
             .apply(RequestOptions().override(it.width, it.height))
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .centerCrop()
             .into(binding.youtubeThumbnail)
     }
 
 
     private fun getThumbnailUtl(videoId: String) =
-        "https://img.youtube.com/vi/$videoId/maxresdefault.jpg"
+        "https://img.youtube.com/vi/$videoId/maxresdefault.jpg".toUri()
 }
